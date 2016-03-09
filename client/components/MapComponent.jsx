f681bc75822b3a424e89226d459b609deaa9a3f5
Axel Johansson <Axel Johansson>
2016-03-09 12:11:36 +0200

    markereita lisätty ja infoboksiin google maps -linkki

--- client/components/MapComponent.jsx
+++ client/components/MapComponent.jsx
@@ -43,21 +43,24 @@ GoogleMap = React.createClass({
 
     GoogleMaps.ready(this.props.name, function(map) {
       var locations = [
-        ['Majoitus', 60.4400759, 22.2909164],
-        ['Showroom', 60.4485752, 22.2635677]
+        ['Majoitus', 'https://goo.gl/maps/exb3ZLa2s182', 60.4400759, 22.2909164],
+        ['Showroom','https://goo.gl/maps/bvNJuLRuwmo', 60.4485752, 22.2635677],
+        ['Liikennepuisto', 'https://goo.gl/maps/5w2cQDb44FN2', 60.445343, 22.286680],
+        ['Tivoli', 'https://goo.gl/maps/BJ56xnoRDoG2', 60.4473199, 22.2805452],
+        ['Boost', 'https://goo.gl/maps/LXcbWE9Yi4D2', 60.4501433, 22.2915006]
       ];
       var infowindow = new google.maps.InfoWindow();
       var marker, i;
 
-      for (i = 0; i < locations.length; i++) {  
+      for (i in locations) {  
         marker = new google.maps.Marker({
-          position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+          position: new google.maps.LatLng(locations[i][2], locations[i][3]),
           map: map.instance
         });
 
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
           return function() {
-            infowindow.setContent(locations[i][0]);
+            infowindow.setContent('<h3>' + locations[i][0] + '</h3>' + '</p><a href="' + locations[i][1] + '" target="_blank">Google Maps -linkki</a></p>');
             infowindow.open(map.instance, marker);
           }
         })(marker, i));
